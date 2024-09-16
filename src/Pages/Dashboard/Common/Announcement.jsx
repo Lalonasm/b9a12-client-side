a9378dfa6db705ca6c2faaeea94937619fccd187
Lalon Sheikh <you@example.com>
2024-09-16 19:53:32 +0600

    helmet added to dashboard, announcement

--- src/Pages/Dashboard/Common/Announcement.jsx
+++ src/Pages/Dashboard/Common/Announcement.jsx
@@ -1,10 +1,17 @@
+import { Helmet } from "react-helmet-async";
 
 
 const Announcement = () => {
+
     return (
         <div>
-            <h2>Admin Announcement</h2>
-            <p>Your destination for everything running. Get inspired, get connected, and get geared up.</p>
+            <div>
+                <Helmet>
+                    <title>Safoon Estate | announcement </title>
+                </Helmet>
+            </div>
+            <h2 className="text-red-400 font-bold text-5xl">Admin Announcement</h2>
+            <p className="text-green-500 font-semibold text-3xl">Your destination for everything running. Get inspired, <br /> get connected, and get geared up.</p>
 
         </div>
     );
